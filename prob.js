34eab0925d9bfe88a219a2614b7ebdbcafd7ed75
Kamil <117399144+Cr3ativeCod3r@users.noreply.github.com>
2023-11-19 14:43:34 +0100

    Update prob.js

--- prob.js
+++ prob.js
@@ -1,32 +1,39 @@
-
-
-
-for (var i = 0; i < 1; i++) {
-
+function obliczZyski() {
     var spanWartosciKursu = document.getElementsByClassName("total-odds")[0];
-    if (spanWartosciKursu) 
-     {
+
+    if (spanWartosciKursu) {
         var wartoscSpanu = spanWartosciKursu.textContent.trim();
         var wartoscSpanuFloat = parseFloat(wartoscSpanu);
         var kurs = wartoscSpanuFloat;
         var stawka = 100;
 
-        var procent = ((1/wartoscSpanuFloat)*100).toFixed(2);
+        var procent = ((1 / wartoscSpanuFloat) * 100).toFixed(2);
 
-
-        var kursulamek = (procent/100).toFixed(2)
+        var kursulamek = (procent / 100).toFixed(2);
         var proby = 1000;
 
-        var lose = 1-kursulamek
-        var win = kursulamek
+        var lose = 1 - kursulamek;
+        var win = kursulamek;
 
-        var potentialwin = ((kurs*stawka)-stawka)*0.88
+        var potentialwin = ((kurs * stawka) - stawka) * 0.88;
 
-        var profit = win*proby*potentialwin
-        var noprofit = lose * proby * stawka
+        var profit = win * proby * potentialwin;
+        var noprofit = lose * proby * stawka;
 
-        var termin = ((profit-noprofit)/stawka).toFixed(0)
+        var termin = ((profit - noprofit) / stawka).toFixed(0);
 
         alert("szanse jednorazowe: " + procent + "% \n  zysk długoterminowy : " + termin + "%");
     }
 }
+
+var przycisk = document.createElement("button");
+przycisk.textContent = "Oblicz Zyski";
+przycisk.style.position = "fixed";
+przycisk.style.top = "0";
+przycisk.style.left = "0";
+przycisk.style.zIndex = "999";
+przycisk.style.color = "red";
+przycisk.style.width = "300px";
+przycisk.style.height = "300px";
+przycisk.addEventListener("click", obliczZyski);
+document.body.appendChild(przycisk);
