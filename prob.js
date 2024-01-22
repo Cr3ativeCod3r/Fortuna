25d7a4bfdea0bf678188622c0307a7f9c201ca51
Kamil <117399144+Cr3ativeCod3r@users.noreply.github.com>
2024-01-22 14:45:09 +0100

    Update prob.js

--- prob.js
+++ prob.js
@@ -1,39 +1,26 @@
-function obliczZyski() {
-    var spanWartosciKursu = document.getElementsByClassName("total-odds")[0];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twoje Zdjęcie</title>
+    <style>
+        body {
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+        }
 
-    if (spanWartosciKursu) {
-        var wartoscSpanu = spanWartosciKursu.textContent.trim();
-        var wartoscSpanuFloat = parseFloat(wartoscSpanu);
-        var kurs = wartoscSpanuFloat;
-        var stawka = 100;
-
-        var procent = ((1 / wartoscSpanuFloat) * 100).toFixed(2);
-
-        var kursulamek = (procent / 100).toFixed(2);
-        var proby = 1000;
-
-        var lose = 1 - kursulamek;
-        var win = kursulamek;
-
-        var potentialwin = ((kurs * stawka) - stawka) * 0.88;
-
-        var profit = win * proby * potentialwin;
-        var noprofit = lose * proby * stawka;
-
-        var termin = ((profit - noprofit) / stawka).toFixed(0);
-
-        alert("szanse jednorazowe: " + procent + "% \n  zysk długoterminowy : " + termin + "%");
-    }
-}
-
-var przycisk = document.createElement("button");
-przycisk.textContent = "Oblicz Zyski";
-przycisk.style.position = "fixed";
-przycisk.style.top = "0";
-przycisk.style.left = "0";
-przycisk.style.zIndex = "999";
-przycisk.style.color = "red";
-przycisk.style.width = "300px";
-przycisk.style.height = "300px";
-przycisk.addEventListener("click", obliczZyski);
-document.body.appendChild(przycisk);
+        img {
+            max-width: 100%;
+            max-height: 100%;
+        }
+    </style>
+</head>
+<body>
+    <img src="moje.jpg" alt="Twoje Zdjęcie">
+</body>
+</html>
